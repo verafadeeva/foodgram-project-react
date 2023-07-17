eaaa73816e01ca3efd91327e926be83bda44c480
verafadeeva <lunatik.yar@gmail.com>
2023-07-17 18:59:13 +0300

    Base admin set

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from api import models
+
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.UserFollowing)
+class UserFollowAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.RoleUser)
+class RoleUSerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.IngredientAmount)
+class IngredientAmountAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    pass
