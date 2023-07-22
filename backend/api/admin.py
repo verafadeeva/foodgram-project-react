37326e82a5b1f842399926ac0eca45c58d2397c4
verafadeeva <lunatik.yar@gmail.com>
2023-07-22 12:49:14 +0300

    Change models, remove RoleUser model

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -9,34 +9,40 @@ User = get_user_model()
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('username', 'last_name', 'first_name', 'email')
+    list_filter = ('email', 'first_name')
+    search_fields = ('last_name__startswith', 'first_name__startswith')
 
 
 @admin.register(models.UserFollowing)
 class UserFollowAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(models.RoleUser)
-class RoleUSerAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('user', 'following_user')
 
 
 @admin.register(models.Tag)
 class TagAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'color', 'slug')
 
 
 @admin.register(models.Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'measurement_unit')
+    list_filter = ('name', )
+    search_fields = ('name__startswith', )
 
 
 @admin.register(models.IngredientAmount)
 class IngredientAmountAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('ingredient', 'recipe', 'amount')
 
 
 @admin.register(models.Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('author', 'name')
+    list_filter = ('author', 'name', 'tags')
+    fields = ('author', 'name', 'image', 'cooking_time', 'tags', 'favorite')
+    readonly_fields = ('favorite', )
+
+    def favorite(self, obj):
+        result = obj.favorited_for.count()
+        return result
