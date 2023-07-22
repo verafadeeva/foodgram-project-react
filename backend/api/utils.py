e9337ccbaff6b8d1a0791c9a0adea4c3d1a6bf1f
verafadeeva <lunatik.yar@gmail.com>
2023-07-22 12:51:00 +0300

    Add function to create txt file

---
+++ backend/api/utils.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+
+
+def make_text(queryset):
+    output = []
+    output.append('Your shopping list')
+    output.append('\n')
+    for i, obj in enumerate(queryset):
+        line = f'{i+1}. {obj.name}: {obj.total_amount} {obj.measurement_unit}'
+        output.append(line)
+    return '\n'.join(output)
+
+
+def create_txt(queryset):
+    text = make_text(queryset)
+    response = HttpResponse(
+        text,
+        'Content-Type: application/pdf',
+        headers={
+            "Content-Disposition": 'attachment; filename="shopping_list.txt"'
+            }
+        )
+    return response
