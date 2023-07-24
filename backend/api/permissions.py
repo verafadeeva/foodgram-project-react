6114a55d2771da5678c5c76ef12abebeefbe509c
verafadeeva <lunatik.yar@gmail.com>
2023-07-24 11:48:01 +0300

    Fixed permission (reviewr's comments)

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -2,18 +2,14 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsAuthorOrReadOnly(BasePermission):
-    """
-    Object-level permission to only allow authors of an object to edit it.
-    Read-only request for any users.
-    """
     def has_permission(self, request, view):
-        return bool(
+        return (
             request.method in SAFE_METHODS
             or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-
-        return obj.author == request.user
+        return (
+            request.method in SAFE_METHODS
+            or obj.author == request.user
+        )
