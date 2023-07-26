c3c5555a1d0347d63312ba7bd54a30dbf3d34f4e
verafadeeva <lunatik.yar@gmail.com>
2023-07-26 11:31:30 +0300

    Add command bulk_create to load ingredients

--- backend/api/management/commands/loadingredients.py
+++ backend/api/management/commands/loadingredients.py
@@ -15,17 +15,17 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         data_file = Path(options['path'])
+        ingredients = []
         try:
             with data_file.open('r') as file:
                 reader = csv.reader(file)
                 for row in reader:
-                    try:
-                        Ingredient.objects.create(
-                            name=row[0],
-                            measurement_unit=row[1]
-                        )
-                    except IntegrityError:
-                        self.stdout.write(f'Ingredient "{row[0]}" exist')
+                    obj = Ingredient(name=row[0], measurement_unit=row[1])
+                    ingredients.append(obj)
+                try:
+                    Ingredient.objects.bulk_create(ingredients)
+                    self.stdout.write('Data loaded successfully')
+                except IntegrityError:
+                    self.stdout.write(f'"{obj.name}" exist')
         except FileNotFoundError:
             raise CommandError('File not found')
-        self.stdout.write('Data loaded successfully')
